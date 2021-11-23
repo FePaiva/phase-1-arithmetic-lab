8bf59ec353f93974782bf3cf61cb9a179b8f5469
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-11-22 22:33:17 -0800

    commit tree 424c4b5eaf6cac3c837239cc850e79465ace57c2

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 12
+let num4 = 3
+const mod = num3 / num4
+
+let max = Math.max(0, 5, 10, 15, 20);
